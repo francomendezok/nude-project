3d5ecee26f0b9d525480f9826c1a3ddc72ed17f5
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2024-05-01 10:56:52 -0300

    Nude Project was Born

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import '../styles/app.scss'
 import '../styles/reset.scss'
-import Diablo from '../images/diablo.avif'
 import nude from '../api/nude.json'
 
 const uniqueImageUrls = new Set();
@@ -17,15 +16,6 @@ const uniqueProducts = nude.filter(product => {
 
 export default function Home () {
     return (
-        <div className="home">
-            {/* <img src={Diablo} alt="Diablo 3 Background" />
-            <Link to='shop'>Games</Link>
-            <Link to='cart'>Cart</Link>
-            <Link to='help'>Help</Link> */}
-            {uniqueProducts.map(product => (
-                <div className="box" key={product.id}>
-                    <img src={product.images_urls[0]} alt={product.title} />
-                </div>
-            ))}        </div>
+        <h1>Hola</h1>
     )
 }
