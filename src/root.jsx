dc8c18697b966bd285229f9b74f0c1d3a3fa0057
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2024-05-11 19:11:16 -0300

    Footer

--- src/root.jsx
+++ src/root.jsx
@@ -23,6 +23,45 @@ export default function Root () {
                 </div>
             </nav>
             <Outlet />
+            <footer id="footer-root">
+                <div className="footer-divs">
+                    <img src={logo} alt="" />
+                    <p>help@nude-project.com</p>
+                    <section className="flex">
+                        <img src="#" alt="" />
+                        <img src="#" alt="" />
+                        <img src="#" alt="" />
+                        <img src="#" alt="" />
+                        <img src="#" alt="" />
+                    </section>
+                </div>
+                <div className="footer-divs">
+                    <p>BRAND</p>
+                    <a href="">STORES</a>
+                    <a href="">CAREERS</a>
+                    <a href="">ABOUT US</a>
+                </div>
+                <div className="footer-divs">
+                    <p>BORING STUFF</p>
+                    <a href="">LEGAL NOTICE</a>
+                    <a href="">PRIVACY POLICY</a>
+                    <a href="">TERMS</a>
+                </div>
+                <div className="footer-divs">
+                    <p>SUPPORT</p>
+                    <a href="">RETURNS</a>
+                    <a href="">FAQ</a>
+                    <a href="">CONTACT</a>
+                </div>
+                <div className="footer-divs">
+                    <p>JOIN THE CULT</p>
+                    <p>SIGN UP TO OUR EMAIL LIST AND GET 10% OFF YOUR FIRST ORDER</p>
+                    <div>
+                        <input type="email" name="email" id=""  placeholder="Email address"/>
+                        <button type="submit">JOIN</button>
+                    </div>
+                </div>
+            </footer>
         </div>
 
     )
