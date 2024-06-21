408ec340247293bebc9d0f22d0bcb952c3f914d3
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2024-06-21 16:50:34 -0300

    Checkout layout

--- src/cart/Checkout.jsx
+++ src/cart/Checkout.jsx
@@ -1,10 +1,11 @@
 export default function Checkout () {
+
     return (
         <main id="checkout-main">
             <section id="fill-info-checkout">a</section>
             <section id="brief-cart-checkout">
                 <div className="brief-inner-box">
-                    <div>cart</div>
+                    <div className="cart-brief">cart</div>
                     <div className="apply-code-brief">
                         <input type="text" placeholder="Discount code or gift card" />
                         <button>Apply</button>
@@ -15,13 +16,13 @@ export default function Checkout () {
                             <p>Shipping</p>
                         </div>
                         <div>
-                            <p>€391.20</p>
+                            <p className="text-white text-right">€391.20</p>
                             <p>Calculated at next step</p>
                         </div>
                     </div>
                     <div className="total-brief">
                         <p>Total</p>
-                        <p>EUR €391.20</p>
+                        <p>EUR <span>€391.20</span></p>
                     </div>
                 </div>
             </section>
